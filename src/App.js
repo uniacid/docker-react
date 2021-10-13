f8b0d75d5287672f59732f3f2cd1557f67ae49f7
Steven Gonzalez <uniacid@gmail.com>
2021-10-13 02:18:43 -0400

    Change text

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there!</p>
+        <p>I was changed on the feature branch</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed on the feature branch
+          Learn React
         </a>
       </header>
     </div>
